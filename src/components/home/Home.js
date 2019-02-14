6cf886f14067372dbe63f3a2e1fe5939a351a791
Benjamin Dally <benjamin.dally@icloud.com>
2019-02-13 16:24:00 -0800

    added a basic text area

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,9 +4,19 @@ import "./home.css";
 class Home extends Component {
   render() {
     return (
-      <div className="home_page_main_wrapper">
-        <div className="call_to_action">
-          <p className="call_to_action_words">Join Our Family</p>
+      <div>
+        <div className="home_page_main_wrapper">
+          <div className="call_to_action">
+            <p className="call_to_action_words">Join Our Family</p>
+          </div>
+        </div>
+        <div>
+          <div className="basic_text_area">
+            <h1>
+              Welcome! We are so happy that you are thinking of us for your
+              childcare needs.
+            </h1>
+          </div>
         </div>
       </div>
     );
