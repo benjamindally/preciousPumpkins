47c045fedfb02c46cba0dfa7198ab97f1415f2e0
Benjamin Dally <benjamin.dally@icloud.com>
2019-03-21 16:24:40 -0700

    abstracted spaceCard

--- src/components/spaces/Spaces.js
+++ src/components/spaces/Spaces.js
@@ -1,32 +1,12 @@
 import React, { Component } from "react";
+import SpaceCard from "./spacecard/spacecard";
 import "./spaces.css";
 
 class Spaces extends Component {
   render() {
     return (
       <div>
-        <div className="basic_space_area">
-          <div className="space_header float_left">
-            <h2 className="text-left float_left">Playroom</h2>
-          </div>
-          <div className="space_picture_wrapper">
-            <img
-              className="space_picture"
-              src={require("../../img/playroom.jpg")}
-            />
-          </div>
-          <div className="space_paragraph">
-            <p>
-              The Playroom is one of the main centers of activity at Precious
-              Pumpkins. In the mornings, one of our excellent staff hosts
-              "Circle Time" where the children are exposed to new letters,
-              songs, and other eduational games. During naptime, the children
-              set their beds here for an afternoon nap. Anytime of the day,
-              children can be found singing, dancing, learning, or playing here.
-              It's really a mult-purpose space!
-            </p>
-          </div>
-        </div>
+        <SpaceCard />
       </div>
     );
   }
