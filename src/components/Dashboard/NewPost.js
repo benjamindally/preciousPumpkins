c4c331a8ef4e5b20e8a0f44efc1643c8644b11d8
Benjamin Dally <benjamin.dally@icloud.com>
2019-04-10 11:19:08 -0700

    working on newposts and adding image drops

--- src/components/Dashboard/NewPost.js
+++ src/components/Dashboard/NewPost.js
@@ -1,4 +1,10 @@
 import React, { Component } from "react";
+import Dropzone from "react-dropzone";
+import request from "superagent";
+
+const CLOUDINARY_UPLOAD_PRESET = "preciousPumpkins";
+const CLOUDINARY_UPLOAD_URL =
+  "https://api.cloudinary.com/v1_1/ddjxtap40/image/upload";
 
 export default class NewPost extends Component {
   state = {
@@ -6,6 +12,8 @@ export default class NewPost extends Component {
     bodyText: "",
     image: "",
     sticky: false,
+    uploadedFileClourdinaryURL: "",
+    uploadedFile: "",
   };
 
   handleChange = event => {
@@ -14,6 +22,30 @@ export default class NewPost extends Component {
       [stateName]: event.target.value,
     });
   };
+
+  onImageDrop = files => {
+    this.setState({ uploadedFile: files[0] });
+  };
+
+  handleImageUpload(file) {
+    let upload = request
+      .post(CLOUDINARY_UPLOAD_URL)
+      .field("upload_preset", CLOUDINARY_UPLOAD_PRESET)
+      .field("file", file);
+
+    upload.end((err, response) => {
+      if (err) {
+        alert("Something went wrong. Please try again.");
+        console.error(err);
+      }
+
+      if (response.body.secure_url !== "") {
+        this.setState({
+          uploadedFileCloudinaryUrl: response.body.secure_url,
+        });
+      }
+    });
+  }
 
   render() {
     return (
@@ -42,6 +74,34 @@ export default class NewPost extends Component {
             />
           </label>
         </form>
+        {/* <Dropzone
+          multiple={false}
+          accept="image/*"
+          onDrop={this.onImageDrop.bind(this)}
+        >
+          <p>Drop an image or click to select a file to upload.</p>
+        </Dropzone> */}
+        <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+          {({ getRootProps, getInputProps }) => (
+            <section>
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <p>Drag 'n' drop some files here, or click to select files</p>
+              </div>
+            </section>
+          )}
+        </Dropzone>
+        <div>
+          {this.state.uploadedFileCloudinaryUrl === "" ? null : (
+            <div>
+              <p>{this.state.uploadedFile.name}</p>
+              <img
+                alt="File Preview"
+                src={this.state.uploadedFileCloudinaryUrl}
+              />
+            </div>
+          )}
+        </div>
       </div>
     );
   }
